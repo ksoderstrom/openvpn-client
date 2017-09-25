2cbe33d7dfb0fdc77a098f91173fd83907883df9
Karl Söderström <karl@karlsoderstrom.com>
2017-09-25 18:33:11 +0200

    added iptables

--- README.md
+++ README.md
@@ -4,11 +4,14 @@ A docker container for connecting to a OpenVPN server.
 
 ## Starting the container
 
-    sudo docker run -d
+    sudo docker run -d \
       --cap-add=NET_ADMIN --device /dev/net/tun \
       --name vpn \
-      -v /etc/localtime:/etc/localtime:ro
+      -v /etc/localtime:/etc/localtime:ro \
       -v </some/apth/vpn>:/vpn \
       -e VPN_USER=<username> \
       -e VPN_PASS=<password> \
+      -e VPN_FIREWALL=<true|false> \
+      -e LOCAL_NETWORK=10.0.0.0/24 \
+      --dns 8.8.8.8 --dns 8.8.4.4 \
       ksoderstrom/openvpn-client
--- vpn.sh
+++ vpn.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -o nounset
+set -e
 
 if [ ! -f /vpn/client.ovpn ];
 then
@@ -13,4 +14,43 @@ chmod 0600 /vpn/authfile
 
 mkdir -p /dev/net
 [[ -c /dev/net/tun ]] || mknod -m 0666 /dev/net/tun c 10 200
+
+ip route | grep -q "127.0.0.1" || ip route add to 127.0.0.1 dev lo
+
+if [ "${VPN_FIREWALL:-false}" = "true" ];
+then
+  echo "Setting up firewall"
+  network=$(ip -o addr show dev eth0 | awk '$3 == "inet" {print $4}')
+  echo "Docker network is $network"
+
+  iptables --version
+  iptables -m owner --help
+
+  iptables -F OUTPUT
+  iptables -P OUTPUT DROP
+  iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+  iptables -A OUTPUT -o lo -j ACCEPT
+  iptables -A OUTPUT -o tap0 -j ACCEPT
+  iptables -A OUTPUT -o tun0 -j ACCEPT
+  iptables -A OUTPUT -d ${network} -j ACCEPT
+  iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
+
+  vpn_port=$(awk '/^remote / && NF ~ /^[0-9]*$/ {print $NF}' /vpn/client.ovpn | grep ^ | head -n 1 || echo 1194)
+  iptables -A OUTPUT -p tcp -m tcp --dport $vpn_port -j ACCEPT
+  iptables -A OUTPUT -p udp -m udp --dport $vpn_port -j ACCEPT
+else
+  echo "Not setting up firewall"
+fi
+
+if [ "${LOCAL_NETWORK:-false}" != "false" ];
+then
+  echo "Setting up local network routing"
+  gateway=$(ip route | awk '/default/ {print $3}')
+  ip route | grep -q "$LOCAL_NETWORK" || ip route add to $LOCAL_NETWORK via $gateway dev eth0
+  if [ "${VPN_FIREWALL:-false}" = "true" ];
+  then
+    iptables -A OUTPUT --destination $LOCAL_NETWORK -j ACCEPT
+  fi
+fi
+
 exec sg vpn -c "openvpn --verb 5 --cd /vpn --config /vpn/client.ovpn --auth-user-pass /vpn/authfile"
